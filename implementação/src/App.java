0b98f8e907bb47c227c75b17476a781cbf97e6b8
Pedro <pedroluizns@hotmail.com>
2025-09-03 17:18:19 -0300

    feat métodos para criação da interface

--- implementação/src/App.java
+++ implementação/src/App.java
@@ -1,5 +1,6 @@
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import Business.Pessoa.Aluno;
 import Business.Pessoa.Professor;
@@ -12,6 +13,7 @@ import Data.DAO.SecretariaDAO;
 import Enum.TipoAcesso;
 
 public class App {
+    static Scanner in = new Scanner(System.in);
     static PrintStream out = System.out;
     static SecretariaDAO secretariaDAO = SecretariaDAO.getDAO();
     static ProfessorDAO professorDAO = ProfessorDAO.getDAO();
@@ -67,6 +69,74 @@ public class App {
 
     public static void main(String[] args) {
         inicializador();
-        out.println(direcionaAutenticacao(TipoAcesso.SECRETARIA, "954617;alice"));
+
+        String header = "Sistema EMA de matrículas";
+        String[] opcoes = {
+                "1 - Acesso para secretária.",
+                "2 - Acesso para professor.",
+                "3 - Acesso para aluno.",
+                "0 - Encerrar sistema.",
+        };
+        int flag;
+        do {
+            imprimirMenu(header, opcoes);
+            flag = in.nextInt();
+
+        } while (flag != 0);
+    }
+
+    static void imprimirMenu(String header, String[] opcoes) {
+        out.println(gerarHeaderMenu(header));
+        for (String opc : opcoes) {
+            out.println(preencherLinha(opc));
+        }
+        out.print(solicitarInput());
+    }
+    static String gerarHeaderMenu(String header) {
+        StringBuilder linha = new StringBuilder();
+        final int MAX_CARACTERES = 45;
+        int blanks = MAX_CARACTERES - (header.length() + 6);
+
+        linha.append("#".repeat(MAX_CARACTERES))
+                .append("\n");
+
+        linha.append("###");
+        linha.append(" ".repeat(Math.max(0, blanks / 2)));
+        linha.append(header);
+        linha.append(" ".repeat(Math.max(0, (blanks / 2 + blanks % 2))));
+        linha.append("###")
+                .append("\n");
+
+        linha.append("#".repeat(MAX_CARACTERES));
+        return linha.toString();
+    }
+    static String preencherLinha(String texto) {
+        StringBuilder linha = new StringBuilder();
+
+        final int MAX_CARACTERES = 45;
+        int blanks = MAX_CARACTERES - (texto.length() + 2);
+
+        linha.append("#");
+        linha.append(" ".repeat(Math.max(0, blanks / 2)));
+        linha.append(texto);
+        linha.append(" ".repeat(Math.max(0, (blanks / 2 + blanks % 2))));
+        linha.append("#");
+
+        return linha.toString();
+    }
+    static String solicitarInput() {
+        String texto = "Digite a opção desejada:";
+        StringBuilder linha = new StringBuilder();
+
+        final int MAX_CARACTERES = 45;
+        int hashtags = MAX_CARACTERES - (texto.length() + 3);
+
+        linha.append("#".repeat(MAX_CARACTERES))
+                .append("\n");
+        linha.append("#".repeat(hashtags))
+                .append(" ");
+        linha.append(texto)
+                .append(" ");
+        return linha.toString();
     }
 }
